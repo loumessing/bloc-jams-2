f4c0d684e2da0c1398ce8f17840462b304ec4673
loumessing <loumessing@gmail.com>
2017-05-14 08:28:01 -0600

    assignment-9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,26 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point')
 
+var animatePoints = function(points) {
     var revealPoint = function(index) {
-
         points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    for (var i=0; i< points.length; i++);{
-        revealPoint(i);
+        points[index].style.transform = "scaleX(1) translateY(0)"
+        points[index].style.msTransform = "scaleX(1) translateY(0)"
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)"
     }
-  };
+    for (var i=0; i< points.length; i++){
+        revealPoint(i)
+    }
+  }
+
+  window.onload = function() {
+    if (window.innerHeight > 950){
+      animatePoints(pointsArray)
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0]
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200
+      window.addEventListener('scroll', function(event){
+    if (document.documentElement.scrollTop || document.scrollTop >= scrollDistance){
+      animatePoints(pointsArray)
+    }
+      })
+}
---
+++ scripts/utlilities.js
@@ -0,0 +1,3 @@
+/*Write a function named forEach in the utilities.js file. For educational purposes, DO NOT use the built-in Array.prototype.forEach function mentioned in the callback resource. The goal is to write your own function named forEach. It should:
+Use a loop to go through all elements in the points array.
+Execute a callback for each element.*/
