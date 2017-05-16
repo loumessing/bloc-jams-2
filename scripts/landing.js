03a3f537ad59a5e2594f2f554a7ceeac93c3b07a
loumessing <loumessing@gmail.com>
2017-05-15 18:17:10 -0600

    assignment-9 stuck

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
 var pointsArray = document.getElementsByClassName('point')
 
-var revealPoint = function (point) {
+var revealPoint = function(point){
     point.style.opacity = 1
     point.style.transform = "scaleX(1) translateY(0)"
     point.style.msTransform = "scaleX(1) translateY(0)"
@@ -16,10 +16,13 @@ var animatePoints = function(points) {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray)
     }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0]
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200
 
-  window.addEventListener('scroll', function(event){
-        if (pointsArray[0].getBoundingClientRect().top <= 500){
-            animatePoints(pointsArray)
+
+  window.addEventListener("scroll", function(event){
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray)
     }
   })
 }
